83aa6b5d00b233e22085b921f22e4e548dd7aa12
Nuraidl <nuradil999@list.ru>
2022-07-01 17:21:32 +0300

    add project:

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger,.header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+  $(".rev_slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      adaptiveHeught: true,
+    },
+  });
+});
